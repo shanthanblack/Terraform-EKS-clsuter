c4ea6d61ce0a166402266a1d7574132fd3b95b23
shanthanblack <43510477+shanthanblack@users.noreply.github.com>
2021-09-22 13:43:56 +0530

    Eks-Cluster-tf files

---
+++ Cluster/provider.tf
@@ -0,0 +1,15 @@
+#
+# Provider Configuration
+#
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Using these data sources allows the configuration to be
+# generic for any region.
+
+# Not required: currently used in conjuction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
